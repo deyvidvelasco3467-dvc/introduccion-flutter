526ff3b73fc7bca40a9d9ee25dcdf342f94f2aa5
deyvidvelasco3467-dvc <deyvidvelasco3467@gmail.com>
2026-06-08 19:07:34 -0400

    Update richtext_screens.dart

---
+++ lib/screens/richtext_screens.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class RichTextWidget extends StatelessWidget {
+  const RichTextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: "Rojo",
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+            ),
+          ),
+          TextSpan(
+            text: "Azul",
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 26,
+              decoration: TextDecoration.underline,
+              fontStyle: FontStyle.italic,
+              shadows: [
+                Shadow(
+                  color: Colors.black,
+                  blurRadius: 15,
+                  offset: Offset(5, 10),
+                ),
+              ],
+            ),
+          ),
+          TextSpan(
+            text: "Verde",
+            style: TextStyle(color: Colors.green, fontSize: 30),
+          ),
+        ],
+      ),
+    );
+  }
+}
